918120b1d9f432248899423e2632a70dc21c2625
Kaveh Nejad <kaveh2009@hotmail.co.uk>
2022-03-26 19:23:06 +0000

    added JSON for object types

--- Recycling/src/scripts/base_object.gd
+++ Recycling/src/scripts/base_object.gd
@@ -1,6 +1,16 @@
 extends Node2D
 
+onready var sprite = get_node("Sprite")
+
 export(float) var speed
+var type
+var image_src
+
+func _ready():
+	set_image()
 
 func _physics_process(_delta):
 	position.y += speed
+
+func set_image():
+	sprite.set_texture(load(image_src))
--- Recycling/src/scripts/item_spawner.gd
+++ Recycling/src/scripts/item_spawner.gd
@@ -2,21 +2,39 @@ extends Node
 
 const Base_Object = preload("res://src/scenes/base_object.tscn")
 
+var OBJECTS_JSON
+
 var screenSize
 
 func _ready():
 	randomize()
+	set_decks_json()
 	screenSize = int(get_viewport().get_visible_rect().size.x)
 
 func _on_Timer_timeout():
-	var object = random_object().instance()
+	var object_json = random_object()
+	var object = Base_Object.instance()
 	object.position.x = random_position()
+	object.type = object_json['type']
+	object.image_src = object_json['image_src']
 	get_parent().add_child(object)
 
 
 func random_object():
-	return Base_Object
+	return OBJECTS_JSON[random_object_index()]
+
+func random_object_index():
+	return randi() % OBJECTS_JSON.size()
 
 func random_position():
 	return randi() % screenSize
 
+func set_decks_json():
+	OBJECTS_JSON = _get_json("res://assets/data/objects/objects.json")
+
+func _get_json(file_path):
+	var file = File.new()
+	file.open(file_path, file.READ)
+	var text = file.get_as_text()
+	file.close()
+	return parse_json(text)
