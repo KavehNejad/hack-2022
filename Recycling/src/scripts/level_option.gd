f4a23b167e0a888e4fa5a2e7b8ce922451e243da
allegrasantoro <allegrasantoro.cs@gmail.com>
2022-03-27 10:42:30 +0100

    Made level select prettier

--- Recycling/src/scripts/level_option.gd
+++ Recycling/src/scripts/level_option.gd
@@ -1,12 +1,22 @@
-extends Button
+extends TextureButton
 
 signal level_selected
 
 var level_name = "some"
 
 func _ready():
-	text = level_name
+	show_image()
 
 
 func _on_Button_pressed():
 	emit_signal("level_selected", level_name)
+
+func show_image():
+	if (level_name == "Level 1"):
+		set_normal_texture(load("res://assets/images/level_preview/1.png"))
+	elif (level_name == "Level 2"):
+		set_normal_texture(load("res://assets/images/level_preview/2.png"))
+	elif (level_name == "Level 3"):
+		set_normal_texture(load("res://assets/images/level_preview/3.png"))
+	elif (level_name == "Level 4"):
+		set_normal_texture(load("res://assets/images/level_preview/4.png"))
