875b613e708ca4eec9cd2a1e3f8d661c187781f2
Kaveh Nejad <kaveh2009@hotmail.co.uk>
2022-03-27 08:42:48 +0100

    added more descriptions

--- Recycling/src/scripts/object_info.gd
+++ Recycling/src/scripts/object_info.gd
@@ -3,7 +3,7 @@ extends Control
 
 func set_up_object(object, bin):
 	$description.text = object['description']
-	$name_label.text = object['type']
+	$name_label.text = object['name']
 	$object_sprite.set_texture(load(object['image_src']))
 	if bin == null:
 		$bin_sprite.visible = false
