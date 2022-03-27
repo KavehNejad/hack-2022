53c23413b27468920c16893693d56ce2769a6106
Kaveh Nejad <kaveh2009@hotmail.co.uk>
2022-03-27 06:50:43 +0100

    makes bin invisible for waste in info page

--- Recycling/src/scripts/object_info.gd
+++ Recycling/src/scripts/object_info.gd
@@ -6,6 +6,8 @@ func set_up_object(object, bin):
 	$name_label.text = object['type']
 	$object_sprite.set_texture(load(object['image_src']))
 	if bin == null:
+		$bin_sprite.visible = false
+		$bin_title.visible = false
 		return
 	$bin_sprite.set_texture(load(bin['image_src']))
 
